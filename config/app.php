f2cf3d8184b8be77846ded476f32430fd0240eec
Jacky Liang <bluelegopiece@hotmail.com>
2015-10-07 17:37:19 -0400

    task #1: removed app.php settings

--- config/app.php
+++ config/app.php
@@ -136,7 +136,6 @@ return [
         Illuminate\Translation\TranslationServiceProvider::class,
         Illuminate\Validation\ValidationServiceProvider::class,
         Illuminate\View\ViewServiceProvider::class,
-        'Illuminate\Html\HtmlServiceProvider',
 
         /*
          * Application Service Providers...
@@ -194,8 +193,6 @@ return [
         'URL'       => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View'      => Illuminate\Support\Facades\View::class,
-        'Html'      => 'Illuminate\Html\HtmlFacade',
-        'Form'      => 'Illuminate\Html\FormFacade',
 
     ],
 
