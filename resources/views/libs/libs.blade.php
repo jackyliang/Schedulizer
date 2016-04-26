466075dbd63060ef09e6322a4f364e50aae36c46
Jacky Liang <bluelegopiece@hotmail.com>
2016-04-26 13:25:53 -0400

    removed nouislider

--- resources/views/libs/libs.blade.php
+++ resources/views/libs/libs.blade.php
@@ -6,7 +6,6 @@
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/pnotify/2.0.0/pnotify.all.min.css">
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/fullcalendar/2.4.0/fullcalendar.min.css">
 
-<script src="http://cdnjs.cloudflare.com/ajax/libs/noUiSlider/8.0.2/nouislider.min.js"></script>
 <script src='http://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js'></script>
 <script src='http://code.jquery.com/ui/1.10.2/jquery-ui.js'></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.10.6/moment.min.js"></script>
