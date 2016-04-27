6fff0dadba7836aec1c5061c2c4f0d298149b070
Jacky Liang <bluelegopiece@hotmail.com>
2016-04-26 21:19:03 -0400

    uncommented the js libraries. woops

--- resources/views/js/libs.blade.php
+++ resources/views/js/libs.blade.php
@@ -1,6 +1,6 @@
-{{--<script src='http://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.1/js/bootstrap.min.js'></script>--}}
+<script src='http://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.1/js/bootstrap.min.js'></script>
 <script src='http://code.jquery.com/ui/1.10.2/jquery-ui.js'></script>
-{{--<script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-material-design/0.3.0/js/material.min.js"></script>--}}
+<script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-material-design/0.3.0/js/material.min.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-material-design/0.3.0/js/ripples.min.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/pnotify/2.0.0/pnotify.all.min.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/fullcalendar/2.4.0/fullcalendar.min.js"></script>
