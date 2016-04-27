d02cad8857f666f0e903bf085192486e8093cda4
Jacky Liang <bluelegopiece@hotmail.com>
2016-04-26 21:39:36 -0400

    undid more mistakes

--- resources/views/js/libs.blade.php
+++ resources/views/js/libs.blade.php
@@ -1,11 +1,8 @@
 <script src='http://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.1/js/bootstrap.min.js'></script>
-<script src='http://code.jquery.com/ui/1.10.2/jquery-ui.js'></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-material-design/0.3.0/js/material.min.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-material-design/0.3.0/js/ripples.min.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/pnotify/2.0.0/pnotify.all.min.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/fullcalendar/2.4.0/fullcalendar.min.js"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.10.6/moment.min.js"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/datejs/1.0/date.min.js"></script>
 
 <script>
     $(function() {
