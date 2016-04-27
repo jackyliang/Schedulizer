e5626a7f60ac470c6880978251e380bb72f37d99
Jacky Liang <bluelegopiece@hotmail.com>
2016-04-26 20:37:46 -0400

    added donate button

--- resources/views/libs/libs.blade.php
+++ resources/views/libs/libs.blade.php
@@ -7,8 +7,5 @@
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/fullcalendar/2.4.0/fullcalendar.min.css">
 
 <script src='http://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js'></script>
-<script src='http://code.jquery.com/ui/1.10.2/jquery-ui.js'></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.10.6/moment.min.js"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/datejs/1.0/date.min.js"></script>
 <!-- Fonts -->
 <link href='http://fonts.googleapis.com/css?family=Roboto:400,300' rel='stylesheet' type='text/css'>
--- resources/views/partials/footer-sched.blade.php
+++ resources/views/partials/footer-sched.blade.php
@@ -4,6 +4,7 @@
     </p>
     <h6 class="text-muted text-center">
             <a href="http://www.jackyliang.com/contact-jacky/"><span class="glyphicon glyphicon glyphicon-thumbs-up"></span> Contact</a> |
-            <a href="{{ URL('about') }}"><span class="glyphicon glyphicon-info-sign"></span> About</a>
+            <a href="{{ URL('about') }}"><span class="glyphicon glyphicon-info-sign"></span> About</a> |
+            <a href="http://paypal.me/jackyliangcorp/10"><span class="glyphicon glyphicon-heart-empty"></span> Donate</a>
     </h6>
 </div>
