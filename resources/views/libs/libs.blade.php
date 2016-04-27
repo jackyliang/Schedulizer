371f854b892a3b8980eda554e44f14f827d12060
Jacky Liang <bluelegopiece@hotmail.com>
2016-04-27 00:44:18 -0400

    added fontawesome and social buttons

--- resources/views/libs/libs.blade.php
+++ resources/views/libs/libs.blade.php
@@ -5,6 +5,7 @@
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-material-design/0.3.0/css/ripples.min.css">
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/pnotify/2.0.0/pnotify.all.min.css">
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/fullcalendar/2.4.0/fullcalendar.min.css">
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.6.1/css/font-awesome.min.css">
 
 <script src="http://cdnjs.cloudflare.com/ajax/libs/noUiSlider/8.0.2/nouislider.min.js"></script>
 <script src='http://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js'></script>
--- resources/views/schedulizer/schedule-panel.blade.php
+++ resources/views/schedulizer/schedule-panel.blade.php
@@ -6,6 +6,12 @@
             <a data-direction="left" class="btn btn-xs btn-primary toggle-schedules mdi-hardware-keyboard-arrow-left"></a>
             <a data-direction="right" class="btn btn-xs btn-primary toggle-schedules mdi-hardware-keyboard-arrow-right"></a>
         </div>
+        <a class="btn btn-xs btn-raised pull-right" href="/schedule/save">
+            <i class="fa fa-facebook-official" aria-hidden="true"></i> Share to Facebook</a>
+        <a class="btn btn-xs btn-raised pull-right" href="/schedule/save">
+            <i class="fa fa-link" aria-hidden="true"></i> Save link </a>
+        <a class="btn btn-xs btn-raised pull-right" href="#">
+            <i class="fa fa-file-image-o"></i> Save as Image</a>
     </div>
     <div id="schedule" class="panel-body panel-options">
         <div id="calendar"></div>
